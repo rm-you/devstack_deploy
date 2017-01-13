70559de0a21526da2e9e6444cdd017d00a6b5d59
Adam Harwell <flux.adam@gmail.com>
2017-01-12 16:08:00 -0800

    Disable ipv6 in devstack

--- stackme.sh
+++ stackme.sh
@@ -52,11 +52,11 @@ EOF
 touch /etc/sysconfig/iptables
 
 # Fix ipv6 support
-sed -i 's/net.ipv6.conf.all.disable_ipv6=1/net.ipv6.conf.all.disable_ipv6=0/' /etc/sysctl.conf
-sed -i 's/net.ipv6.conf.default.disable_ipv6=1/net.ipv6.conf.default.disable_ipv6=0/' /etc/sysctl.conf
-sed -i 's/net.ipv6.conf.lo.disable_ipv6=1/net.ipv6.conf.lo.disable_ipv6=0/' /etc/sysctl.conf
-sysctl -p /etc/sysctl.conf
-echo 'precedence ::ffff:0:0/96  100' >> /etc/gai.conf
+#sed -i 's/net.ipv6.conf.all.disable_ipv6=1/net.ipv6.conf.all.disable_ipv6=0/' /etc/sysctl.conf
+#sed -i 's/net.ipv6.conf.default.disable_ipv6=1/net.ipv6.conf.default.disable_ipv6=0/' /etc/sysctl.conf
+#sed -i 's/net.ipv6.conf.lo.disable_ipv6=1/net.ipv6.conf.lo.disable_ipv6=0/' /etc/sysctl.conf
+#sysctl -p /etc/sysctl.conf
+#echo 'precedence ::ffff:0:0/96  100' >> /etc/gai.conf
 
 # Grab dib and patches
 if [ -n "$DIB_PATCH" ]; then
